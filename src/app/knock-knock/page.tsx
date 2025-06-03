ff1c47dbf98ad643a7be470c1f8ed3a102633a6d
Vishwesh Patil <vishveshpatil99@gmail.com>
2025-06-03 16:05:21 +0400

    Remove unused imports and fix build errors

--- src/app/knock-knock/page.tsx
+++ src/app/knock-knock/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import Link from "next/link";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { FaHome, FaArrowRight } from "react-icons/fa";
 
